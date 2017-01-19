5489d86910363d909112b7891c795eae76b82ea3
Rizwan Hasan <rizwan.hasan486@gmail.com>
2017-01-20 01:29:48 +0600

    Update and rename wget-lfs-packages-7.10 to wget-lfs-packages-7.10.sh

--- LFS-Packages-7.10/wget-lfs-packages-7.10.sh
+++ LFS-Packages-7.10/wget-lfs-packages-7.10.sh
@@ -5,5 +5,4 @@
 # Contact: http://www.facebook.com/rizwan.hasan.106
 # Linux From Scratch 7.10 LiveCD Download From http://lfs-livecd.sourceforge.io 
 # Github: https://github.com/Rizwan-Hasan/lfs-livecd
-
 wget -c http://ftp.lfs-matrix.net/pub/lfs/lfs-packages/lfs-packages-7.10.tar
